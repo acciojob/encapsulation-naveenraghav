06b83dbd817e28ed71dfb44b83291d257102384b
Naveen <naveenraghav807@gmail.com>
2023-06-15 10:09:53 +0530

    encapsulationSolution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+	public static void main(String[] args) {
+		RWOnly obj = new RWOnly();
+		obj.setName("Krishna"); // Setting value using setter function
+		System.out.println(obj.getName()); // Accessing value using getter function
+	}
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class RWOnly {
+
+	private String name; // Private member variable
+
+	// Getter function
+	public String getName() {
+		return name;
+	}
+
+	// Setter function
+	public void setName(String name) {
+		this.name = name;
+	}
     
 }
